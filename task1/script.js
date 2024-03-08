86c4071ab5288eea9a2321d87010a2c19eaec406
arnelpajota <143138111+arnelpajota@users.noreply.github.com>
2024-03-08 19:48:35 +0800

    Add files via upload

---
+++ task1/script.js
@@ -0,0 +1,23 @@
+
+var myString = "Hello, ";
+var myNumber = 5;
+var myBoolean = true;
+
+
+var additionResult = myNumber + 10;
+var subtractionResult = myNumber - 3;
+var multiplicationResult = myNumber * 2;
+var divisionResult = myNumber / 2;
+
+
+if (myNumber > 4) {
+    console.log(myString + "the number is Greater than 4");
+} else {
+    console.log(myString + "the number is Less Than 4 ");
+}
+
+
+console.log("Addition Result:", additionResult);
+console.log("Subtraction Result:", subtractionResult);
+console.log("Multiplication Result:", multiplicationResult);
+console.log("Division Result:", divisionResult);
